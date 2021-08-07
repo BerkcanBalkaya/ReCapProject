6fdde760c8f4bba0b62bc7818a95b0b0cc102352
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-08-08 00:28:46 +0300

    Services has been written for newly added entities

---
+++ Business/Abstract/ICustomerService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface ICustomerService
+    {
+        IResult Add(Customer customer);
+        IResult Update(Customer customer);
+        IResult Delete(Customer customer);
+
+        IDataResult<List<Customer>> GetAll();
+        IDataResult<Customer> GetById(int id);
+    }
+}
---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface IUserService
+    {
+        IResult Add(User user);
+        IResult Update(User user);
+        IResult Delete(User user);
+        IDataResult<List<User>> GetAll();
+        IDataResult<User> GetById(int id);
+    }
+}
