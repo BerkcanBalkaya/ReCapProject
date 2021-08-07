c9daefa9de1290335c2c2943eb28d6808cd891ac
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-08-08 00:25:30 +0300

    Message structure has been added

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string BrandAdded = "Brand added";
+        public static string BrandUpdated = "Brand updated";
+        public static string BrandDeleted = "Brand deleted";
+        public static string BrandNameInvalid = "Brand's name is invalid";
+        public static string BrandsListed = "Brands listed";
+        public static string BrandListedById = "Brand listed by id";
+
+        public static string ColorAdded = "Color added";
+        public static string ColorUpdated = "Color updated";
+        public static string ColorDeleted = "Color deleted";
+        public static string ColorNameInvalid = "Color's name is invalid";
+        public static string ColorsListed = "Colors listed";
+        public static string ColorListedById = "Color listed by id";
+
+        public static string CarAdded = "Car added";
+        public static string CarUpdated = "Car updated";
+        public static string CarDeleted = "Car deleted";
+        public static string CarNameOrPriceInvalid = "Car's name or price is invalid";
+        public static string CarsListed = "Cars listed";
+        public static string CarListedById = "Car listed by id";
+
+        public static string CustomerAdded = "Customer added";
+        public static string CustomerUpdated = "Customer updated";
+        public static string CustomerDeleted = "Customer deleted";
+        public static string CustomerCompanyNameInvalid = "Customer's company name is invalid";
+        public static string CustomersListed = "Customers listed";
+        public static string CustomerListedById = "Customer listed by id";
+
+        public static string UserAdded = "User added";
+        public static string UserUpdated = "User updated";
+        public static string UserDeleted = "User deleted";
+        public static string UserInvalid = "User is invalid";
+        public static string UsersListed = "Users listed";
+        public static string UserListedById = "User listed by id";
+
+        public static string RentalAdded = "Rental added";
+        public static string RentalUpdated = "Rental updated";
+        public static string RentalDeleted = "Rental deleted";
+        public static string RentalInvalid = "Rental is invalid";
+        public static string RentalsListed = "Rentals listed";
+        public static string RentalListedById = "Rental listed by id";
+
+        public static string CarDetailListed = "Car detail listed";
+
+
+    }
+}
