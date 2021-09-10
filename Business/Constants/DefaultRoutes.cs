3d22681591b6c82112bca9ece14d86d8ba93d1e9
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-10 21:46:46 +0300

    Added new constant for DefaultRoutes

---
+++ Business/Constants/DefaultRoutes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public class DefaultRoutes
+    {
+        public static string DefaultImageFolder = $"{Directory.GetParent(Directory.GetCurrentDirectory())}\\WebAPI\\wwwroot\\images\\";
+        public static string DefaultImage = $"{DefaultImageFolder}defaultLogo.jpg";
+    }
+}
