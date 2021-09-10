8bbb1f2339711a4a2e6afd90467ffd9a71582c53
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-10 21:44:21 +0300

    Added ICarImageService

---
+++ Business/Abstract/ICarImageService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface ICarImageService
+    {
+        IResult Add(CarImage carImage);
+        IResult Update(CarImage carImage);
+        IResult Delete(CarImage carImage);
+
+        IDataResult<List<CarImage>> GetAll();
+        IDataResult<CarImage> GetByCarId(int carId);
+    }
+}
