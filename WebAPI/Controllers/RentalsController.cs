92aaed8672ac26770da502f211fbf3ad9f785fd8
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-14 12:54:57 +0300

    Refactored

--- WebAPI/Controllers/RentalsController.cs
+++ WebAPI/Controllers/RentalsController.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Business.Abstract;
+﻿using Business.Abstract;
 using Entities.Concrete;
-using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
