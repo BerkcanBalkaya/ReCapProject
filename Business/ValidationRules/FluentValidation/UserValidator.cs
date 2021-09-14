f8e6031c33fe06c0e5f067df48b1f32038afe4a9
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-14 12:29:12 +0300

    Modified for encryption format

--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Core.Entities.Concrete;
-using Entities.Concrete;
+﻿using Core.Entities.Concrete;
 using FluentValidation;
 
 namespace Business.ValidationRules.FluentValidation
