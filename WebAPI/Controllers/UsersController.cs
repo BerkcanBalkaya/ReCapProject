22712ca6bf82d1135e6ff122457cc196c6489109
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-14 12:55:12 +0300

    Refactored

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Business.Abstract;
+﻿using Business.Abstract;
 using Core.Entities.Concrete;
-using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers
 {
