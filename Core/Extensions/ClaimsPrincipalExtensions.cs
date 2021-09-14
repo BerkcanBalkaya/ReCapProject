170f6156cfe9e7296e7685f66eedd4f50f262c85
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-14 12:36:50 +0300

    Added ClaimsPrincipalExtensions

--- Core/Extensions/ClaimsPrincipalExtensions.cs
+++ Core/Extensions/ClaimsPrincipalExtensions.cs
@@ -11,7 +11,6 @@ namespace Core.Extensions
             var result = claimsPrincipal?.FindAll(claimType)?.Select(x => x.Value).ToList();
             return result;
         }
-
         public static List<string> ClaimRoles(this ClaimsPrincipal claimsPrincipal)
         {
             return claimsPrincipal?.Claims(ClaimTypes.Role);
