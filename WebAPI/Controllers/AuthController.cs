79e6d85c7d491898db6240da36c5a58a8d1839c7
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-14 12:54:42 +0300

    Added AuthController

--- WebAPI/Controllers/AuthController.cs
+++ WebAPI/Controllers/AuthController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Business.Abstract;
+﻿using Business.Abstract;
 using Entities.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
