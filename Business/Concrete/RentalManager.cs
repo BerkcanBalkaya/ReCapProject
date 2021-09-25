f7d153254844bc86a784ba4a49e194c62b0fa73b
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-25 11:20:20 +0300

    Modified for newly added service method

--- Business/Concrete/RentalManager.cs
+++ Business/Concrete/RentalManager.cs
@@ -11,6 +11,7 @@ using Core.Utilities.BusinessRules;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace Business.Concrete
 {
@@ -62,6 +63,11 @@ namespace Business.Concrete
         public IDataResult<Rental> GetById(int id)
         {
             return new SuccessDataResult<Rental>(_rentalDal.Get(r => r.Id == id), Messages.RentalListedById);
+        }
+
+        public IDataResult<List<RentalDetailDto>> GetRentalDetails()
+        {
+            return new SuccessDataResult<List<RentalDetailDto>>(_rentalDal.GetRentalDetails(), Messages.CarDetailListed);
         }
 
         public IResult isCarAvailable(Rental rental)
