1db7dd28b032906add8cbe15ac7fdadd4514f34b
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-28 15:25:46 +0300

    Added CreditCardManager

---
+++ Business/Concrete/CreditCardManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    class CreditCardManager:ICreditCardService
+    { 
+        ICreditCardDal _creditCardDal;
+
+        public CreditCardManager(ICreditCardDal creditCardDal)
+        {
+            _creditCardDal = creditCardDal;
+        }
+
+        public IResult Add(CreditCard creditCard)
+        {
+            _creditCardDal.Add(creditCard);
+            return new SuccessResult(Messages.CreditCardAdded);
+        }
+
+        public IResult Update(CreditCard creditCard)
+        {
+            _creditCardDal.Update(creditCard);
+            return new SuccessResult(Messages.CreditCardUpdated);
+        }
+
+        public IResult Delete(CreditCard creditCard)
+        {
+            _creditCardDal.Delete(creditCard);
+            return new SuccessResult(Messages.CreditCardDeleted);
+        }
+
+        public IDataResult<List<CreditCard>> GetAll()
+        {
+            return new SuccessDataResult<List<CreditCard>>(_creditCardDal.GetAll(), Messages.CreditCardListed);
+        }
+
+        public IDataResult<CreditCard> GetById(int id)
+        {
+            return new SuccessDataResult<CreditCard>(_creditCardDal.Get(c => c.Id == id), Messages.CreditCardListed);
+        }
+
+        public IDataResult<List<CreditCard>> GetByUserId(int userId)
+        {
+            return new SuccessDataResult<List<CreditCard>>(_creditCardDal.GetAll(c=>c.UserId==userId),Messages.CreditCardListed);
+        }
+    }
+}
