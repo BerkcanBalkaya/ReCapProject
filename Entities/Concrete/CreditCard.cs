94608189ded506caaab1393be0679d0aee28f168
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-28 15:32:34 +0300

    Added CreditCard

---
+++ Entities/Concrete/CreditCard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class CreditCard:IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string CreditCardType { get; set; }
+        public string CardNumber { get; set; }
+        public string CVC { get; set; }
+        public string ExpirationDateMonth { get; set; }
+        public string ExpirationDateYear { get; set; }
+
+    }
+}
