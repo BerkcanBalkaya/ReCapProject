1e72c0be3d7b5d881ba01c541a82a1b42a467d17
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-28 15:33:03 +0300

    Added FakePaymentInfoDto

---
+++ Entities/DTOs/FakePaymentInfoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Entities.Concrete;
+
+namespace Entities.DTOs
+{
+    public class FakePaymentInfoDto:IDto
+    {
+        public CreditCard CreditCard { get; set; } 
+        public Rental Rental { get; set; } 
+    }
+}
