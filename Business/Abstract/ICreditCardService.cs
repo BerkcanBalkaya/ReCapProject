973f431aea8718efa97613896c2348bfed2db3ba
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-28 15:22:21 +0300

    Added ICreditCardService

---
+++ Business/Abstract/ICreditCardService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+    public interface ICreditCardService
+    {
+        IResult Add(CreditCard creditCard);
+        IResult Update(CreditCard creditCard);
+        IResult Delete(CreditCard creditCard);
+        IDataResult<List<CreditCard>> GetAll();
+        IDataResult<CreditCard> GetById(int id);
+        IDataResult<List<CreditCard>> GetByUserId(int userId);
+    }
+}
