d6c8a71346bdc2ff464012cb531c5998fb84ec6d
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-28 15:22:54 +0300

    Modified Add and Update methods for IFromFile parameter

--- Business/Abstract/ICarImageService.cs
+++ Business/Abstract/ICarImageService.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Core.Utilities.Results;
+﻿using Core.Utilities.Results;
 using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 
 namespace Business.Abstract
 {
     public interface ICarImageService
     {
-        IResult Add(CarImage carImage);
-        IResult Update(CarImage carImage);
+        IResult Add(IFormFile formFile,CarImage carImage);
+        IResult Update(IFormFile formFile, CarImage carImage);
         IResult Delete(CarImage carImage);
-
         IDataResult<List<CarImage>> GetAll();
         IDataResult<List<CarImage>> GetByCarId(int carId);
     }
