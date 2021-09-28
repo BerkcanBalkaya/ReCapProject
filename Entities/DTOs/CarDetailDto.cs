3f075c37c671c23792b88a4208dad88444f9e7c2
Berkcan Balkaya <berkcanbalkaya2010@gmail.com>
2021-09-28 15:32:51 +0300

    Modified by adding ModelYear property

--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -9,10 +9,12 @@ namespace Entities.DTOs
 {
     public class CarDetailDto:IDto
     {
+        //TODO: Car entitisinde bulunan model year burda yok haberin ola eklersin belki
         public int CarId { get; set; }
         public string CarName { get; set; }
         public string BrandName { get; set; }
         public string ColorName { get; set; }
         public decimal DailyPrice { get; set; }
+        public int ModelYear { get; set; }
     }
 }
